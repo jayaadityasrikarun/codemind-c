1939e2c55afdf1c3a54547c79e8e1b98611363fe
jayaadityasrikarun <119720478+jayaadityasrikarun@users.noreply.github.com>
2022-12-30 21:20:06 +0530

    #include<stdio.h> int main() {     int n,p=1,s=0;     scanf("%d",&n);     while(n!=0)     {         s=s*10+n%10;         n/=10;     }     printf("%d",s); }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,r,s=0;
+    int n,p=1,s=0;
     scanf("%d",&n);
     while(n!=0)
     {
-        r=n%10;
-        n=n/10;
-        s=s*10+r;
+        s=s*10+n%10;
+        n/=10;
     }
     printf("%d",s);
 }
